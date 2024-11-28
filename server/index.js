eb0c050c94dfd4c0284b39bfb1d58fff9849b31a
youssef marzoug <youssefmarzoug19@gmail.com>
2024-11-28 23:09:13 +0100

    Votre message de commit ici

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 
-	app.use(express.static(path.join(__dirname, '/client/build')));
+	app.use(express.static(path.join(__dirname, '../client/build')));
 
 	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 
