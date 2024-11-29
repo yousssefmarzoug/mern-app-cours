729367f7077fbbf1cfd40370becab98a86f0799d
youssef marzoug <youssefmarzoug19@gmail.com>
2024-11-29 14:27:39 +0100

    url:5000

--- client/src/redux/utils/api.js
+++ client/src/redux/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000', // URL dynamique
+  baseURL: 'http://localhost:3000', // URL dynamique
   headers: {
     'Content-Type': 'application/json',
   },
